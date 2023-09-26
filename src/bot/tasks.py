2d08f9cab04deac790c57e410cd59717398aa84c
OleksandrYanchar <oleksandryancharr@gmail.com>
2023-09-26 12:17:58 +0300

    Init: craetes thread of scrapers

---
+++ src/bot/tasks.py
@@ -0,0 +1,35 @@
+import csv
+from src.configs.settings import csv_filename
+import threading
+from src.bot.parcesers import jobs_launcher, joobl_launcher, workua_launcher
+
+def crate_table():
+    with open(csv_filename, "a", newline="", encoding="utf-8") as file:
+        fieldnames = ["vacancy_title", "vacancy_link"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+
+async def thread():
+    funcs = [crate_table, joobl_launcher, jobs_launcher, workua_launcher ]
+
+
+    max_threads = 7
+
+    thread_semaphore = threading.Semaphore(max_threads)
+
+    threads = []
+    for func in funcs:
+        thread = threading.Thread(target=func)
+        threads.append(thread)
+
+    for thread in threads:
+        thread_semaphore.acquire()
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+
+    thread_semaphore.release()
+    print("Всі потоки завершили роботу")
+
