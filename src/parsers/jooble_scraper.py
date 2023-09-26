91e428925d344e71ad908872c803405e14377e69
OleksandrYanchar <oleksandryancharr@gmail.com>
2023-09-26 12:10:43 +0300

    Init: ua.jooble.org scraper class

---
+++ src/parsers/jooble_scraper.py
@@ -0,0 +1,45 @@
+import requests
+import re
+import csv
+from bs4 import BeautifulSoup
+
+class JoobleScraper:
+    def __init__(self, jooble_url, csv_filename):
+        self.page_url = jooble_url
+        self.csv_filename = csv_filename
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        }
+    def check_matches(self, job_title):
+        match = re.search(rf'(?i)(\b(?:python|backend)\b.*\b(?:junior|trainee)\b|\b(?:junior|trainee)\b.*\b(?:python|backend)\b)', job_title)
+        return bool(match)
+
+    def get_vacancies_from_page(self):
+        response = requests.get(self.page_url, headers=self.headers)
+        vacancies_list = []
+
+        try:
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                elements = soup.find_all(class_="jkit_Efecu")
+
+                for element in elements:
+                    job_link = element['href']
+                    job_title = element.get_text(strip=True)
+
+                    if job_link and job_title and self.check_matches(job_title):
+                        vacancies_list.append(f"{job_link} - {job_title}")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+        return vacancies_list
+
+    def save_to_csv(self, job_title, job_link):
+        try:
+            with open(self.csv_filename, "a", newline="", encoding="utf-8") as file:
+                fieldnames = ["vacancy_title", "vacancy_link"]
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                writer.writerow({"vacancy_title": job_title, "vacancy_link": job_link})
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
