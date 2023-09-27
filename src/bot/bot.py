9c934c9a8537206a366e972a7b7f143e95ff15a1
OleksandrYanchar <oleksandryancharr@gmail.com>
2023-09-27 05:34:57 +0300

    UPDATE: add new functions for different scenarios of using

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -5,10 +5,12 @@ from aiogram import Bot, Dispatcher, types, F
 from src.configs.settings import TOKEN, IDS, csv_filename, bot, dp
 from src.bot.tasks import thread
 
+# clears csv file 
 async def clear_file():
     with open(csv_filename, mode='w', encoding='utf-8') as file:
         file.truncate(0)
 
+#reads csv file
 async def read_csv():
     try:
         with open(csv_filename, mode='r', encoding='utf-8') as file:
@@ -19,14 +21,14 @@ async def read_csv():
         print(f"Помилка при обробці CSV файлу: {str(e)}")
         return []
 
-# Функція для відправки повідомлень
+# sends date fron csv file to admins
 async def send_messages():
-    data = await read_csv()  # Очікуємо завершення функції read_csv()
+    data = await read_csv()  
     if not data:
         return
 
-    # Ініціалізуємо змінні для обмеження кількості тайтлів і посилань в одному повідомленні
-    max_items_per_message = 10
+    # values for sending message
+    max_items_per_message = 10 # max amount of vacancies in one message
     current_items = 0
     message_text = ""
 
@@ -34,11 +36,9 @@ async def send_messages():
         title = row.get('vacancy_title', '')
         link = row.get('vacancy_link', '')
 
-        # Додаємо тайтл і посилання до поточного повідомлення
         message_text += f"{title} - {link}\n"
         current_items += 1
 
-        # Якщо досягнуто обмеження, надсилаємо повідомлення та очищуємо змінну
         if current_items >= max_items_per_message:
             for chat_id in IDS:
                 try:
@@ -48,7 +48,6 @@ async def send_messages():
                 except Exception as e:
                     print(f"Помилка відправлення повідомлення в чат з ID {chat_id}: {str(e)}")
 
-            # Очищуємо змінну для нових тайтлів і посилань
             current_items = 0
             message_text = ""
 
@@ -56,20 +55,35 @@ async def send_messages():
         for chat_id in IDS:
             try:
                 await bot.send_message(chat_id, text=message_text)
-                print(f"Залишок повідомлення відправлений в чат з ID: {chat_id}")
+                print(f"rest of messages were sent to chat: {chat_id}")
                 await asyncio.sleep(1)
             except Exception as e:
-                print(f"Помилка відправлення залишку повідомлення в чат з ID {chat_id}: {str(e)}")
+                print(f"EROR of sennding rest of messages to chat {chat_id}: {str(e)}")
 
+#handles /start command to get users ID
 @dp.message(F.text =='/start')
 async def lol(message: types.Message):
-    await message.answer('aga')
+    await message.answer(str(message.from_user.id))
+
+#handles /scrap command to start scrapind websites and sends date to user
+@dp.message(F.text =='/scrap')
+async def scrap(message: types.Message):
+    await bot_tasks()
+
+# creates tasks and order for scraping
+async def bot_tasks():
     await thread()
     await read_csv()  
-    await send_messages()
-    await clear_file()
+    await  send_messages()
+    await  clear_file()
 
+#main function to start poling bot
+async def bot_main():
+    await dp.start_polling(bot,skip_updates=True)
 
-async def main():
-    await dp.start_polling(bot)
+#function for celery task which executes at specifed time and than stops bot
+async def bot_schedule():
+    await bot_tasks()
+    await dp.stop_polling(bot)
+    await dp.start_polling(bot,skip_updates=True)
 
